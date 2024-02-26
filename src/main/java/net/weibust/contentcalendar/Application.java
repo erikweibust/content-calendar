0d4068e1b82636de7435b8d082b8dc0be6d2b238
Erik Weibust <erikweibust@gmail.com>
2024-02-25 21:54:50 -0600

    renamed Spring Application file

--- src/main/java/net/weibust/contentcalendar/Application.java
+++ src/main/java/net/weibust/contentcalendar/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContentCalendarApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("*** in main");
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		System.out.println("*** app should be running!");
 	}
 
