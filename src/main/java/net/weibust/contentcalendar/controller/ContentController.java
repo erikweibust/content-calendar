cac5c38999c9d05211bfd5a7f3c186d38c3516f7
Erik Weibust <erikweibust@gmail.com>
2024-03-05 14:50:04 -0600

    Added new http request and repo changes to get content (roughly 1:21:10 in video)

--- src/main/java/net/weibust/contentcalendar/controller/ContentController.java
+++ src/main/java/net/weibust/contentcalendar/controller/ContentController.java
@@ -1,18 +1,50 @@
 package net.weibust.contentcalendar.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import net.weibust.contentcalendar.model.Content;
 import net.weibust.contentcalendar.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
-        private final ContentCollectionRepository repository;
+    private final ContentCollectionRepository repository;
 
-        public ContentController(ContentCollectionRepository repository) {
-            this.repository = repository;
-        }
+    @Autowired
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    // make a reqest and find all the pieces of content in the system
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
 
 }
--- src/main/java/net/weibust/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/net/weibust/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,28 +1,44 @@
 package net.weibust.contentcalendar.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.annotation.PostConstruct;
 import net.weibust.contentcalendar.model.Content;
+import net.weibust.contentcalendar.model.Status;
+import net.weibust.contentcalendar.model.Type;
 
 import java.util.Optional;
 import java.util.List;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter( c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter( c -> c.id().equals(id)).findFirst();
     }
 
+    public void save(Content content) {
+        contentList.add(content);
+    }
+
+    @PostConstruct
+    private void init() {
+        Content content = new Content(1, "My First Blog Post", "My first blog post", Status.IDEA, Type.ARTICLE, LocalDateTime.now(), null, "null");
+
+        contentList.add(content); 
+    }
 
 }
